198d895a13a3f58bab14335e75945c6d9b7a5902
DanoMAC <dano@cev.com>
2024-11-27 10:21:47 +0100

    Initial Commit

---
+++ RoomManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RoomManager
+//
+//  Created by Alumnos on 27/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
